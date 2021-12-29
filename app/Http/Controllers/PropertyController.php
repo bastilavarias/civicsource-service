c1964271de3f05956585e86e9219b9186d860118
Sebastian <sebastiancurtislavarias@gmail.com>
2021-12-29 23:13:22 +0800

    save progress.

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Property;
 
 class PropertyController extends Controller
 {
-    public function index() {
+    public function show($id)
+    {
+        $property = Property::find($id);
 
+        if ($property) {
+            return customResponse()
+                ->data($property)
+                ->message('Property found.')
+                ->success()
+                ->generate();
+        }
+
+        return customResponse()
+            ->data(null)
+            ->message('Property not found.')
+            ->success()
+            ->generate();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/property/{id}', [
+    \App\Http\Controllers\PropertyController::class,
+    'show',
+]);
